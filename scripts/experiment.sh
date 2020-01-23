82a0fbdd1b9a6b6cd5965d43b1e35e6825e6b492
Josh Nussbaum <joshnuss@gmail.com>
2020-01-23 00:24:59 -0500

    Adds shebang

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ludwig experiment \
   --data_csv input.csv \
   --model_definition_file model_definition.yaml
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ludwig train \
   --data_csv input.csv \
   --model_definition_file model_definition.yaml
